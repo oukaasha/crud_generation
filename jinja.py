491c0567281fb419bed8afc70502df67c5e80ce8
oukaasha <okasha.habib@gmail.com>
2019-05-26 15:13:09 +0500

    edit form and database CUD added. some improvements.

--- jinja.py
+++ jinja.py
@@ -1,5 +1,6 @@
 from jinja2 import Environment, FileSystemLoader
 import os
+import copy
 
 generate_directory = 'generated'
 database_table_name = 'appointment'
@@ -7,6 +8,8 @@ database_table_name = 'appointment'
 main_file = database_table_name + '.blade.php'
 table_file = database_table_name + '_table_partial.blade.php'
 javascript_file = database_table_name + '.js'
+edit_form_file = database_table_name + '_edit_form_partial.blade.php'
+store_and_update_file = 'store_and_update.php'
 model_name = database_table_name.title()
 
 # format => 'column_name': ['html tag', 'type:if_any']
@@ -27,7 +30,7 @@ database_table_structure_orig = {
 	'appointment_scheduled_datetime': ['input', 'text'],
 	'appointment_type_code': ['select'],
 	'appointment_status_code': ['select'],
-	'appointment_update_count': ['input', 'text'],
+	'appointment_updated_count': ['input', 'text'],
 	'service_id': ['select'],
 	'client_id': ['select'],
 	'provider_id': ['select']
@@ -41,50 +44,52 @@ env = Environment( loader = FileSystemLoader(template_dir), lstrip_blocks = True
 main_page = env.get_template('main_page.txt')
 table = env.get_template('table.txt')
 javascript = env.get_template('javascript.txt')
+edit_form = env.get_template('edit_form.txt')
+store_and_update = env.get_template('store_and_update.txt')
  
 ##################################
 #   creating main page
 ##################################
-database_table_structure = database_table_structure_orig
-for column_name in database_table_structure:
+database_table_structure_for_main = copy.deepcopy(database_table_structure_orig)
+for column_name, values_array in database_table_structure_for_main.items():
 	name_array = column_name.split('_')
 	name = ' '.join(name_array).title()
-	database_table_structure[column_name].insert(0, name)
-	if database_table_structure[column_name][1] == 'select':
+	values_array.insert(0, name)
+	if values_array[1] == 'select':
 		name_array[-1] = 'list'
 		name = '_'.join(name_array)
-		database_table_structure[column_name].insert(2, name)
+		values_array.insert(2, name)
 		name_array[-1] = 'item'
 		name = '_'.join(name_array)
-		database_table_structure[column_name].insert(3, name)
+		values_array.insert(3, name)
 		if name_array[0] == model_name.lower():
 			name_array[-1] = 'code'
 		else:
 			name_array[-1] = 'id'
 		name = '_'.join(name_array)
-		database_table_structure[column_name].insert(4, name)
+		values_array.insert(4, name)
 		name_array[-1] = 'name'
 		name = '_'.join(name_array)
-		database_table_structure[column_name].insert(5, name)
+		values_array.insert(5, name)
 
 
 filename = os.path.join(root, generate_directory, main_file)
 with open(filename, 'w') as fh:
   fh.write(main_page.render(
   	model_name = model_name,
-  	database_table_structure = database_table_structure,
+  	database_table_structure = database_table_structure_for_main,
   ))
 
 ##################################
 #   creating table
 ##################################
-database_table_structure = database_table_structure_orig
-for column_name in database_table_structure:
+database_table_structure_for_table = copy.deepcopy(database_table_structure_orig)
+for column_name, values_array in database_table_structure_for_table.items():
 	name_array = column_name.split('_')
 	if name_array[0] == database_table_name:
 		name_array.pop(0)
 	name = ' '.join(name_array).upper()
-	database_table_structure[column_name].insert(0, name)
+	values_array.insert(0, name)
 
 variable_list = database_table_name + '_list'
 variable_item = database_table_name + '_item'
@@ -95,7 +100,7 @@ with open(filename, 'w') as fh:
   	variable_item = variable_item,
   	variable_list = variable_list,
   	database_table_name = database_table_name,
-  	database_table_structure = database_table_structure,
+  	database_table_structure = database_table_structure_for_table,
   ))
 
 ##################################
@@ -105,4 +110,25 @@ filename = os.path.join(root, generate_directory, javascript_file)
 with open(filename, 'w') as fh:
   fh.write(javascript.render(
   	database_table_name = database_table_name,
+  ))
+
+##################################
+#   creating edit form
+##################################
+filename = os.path.join(root, generate_directory, edit_form_file)
+with open(filename, 'w') as fh:
+  fh.write(edit_form.render(
+  	database_table_name = database_table_name,
+  	database_table_structure = database_table_structure_for_main,
+  ))
+
+##################################
+#   creating store and update
+##################################
+filename = os.path.join(root, generate_directory, store_and_update_file)
+with open(filename, 'w') as fh:
+  fh.write(store_and_update.render(
+  	model_name = model_name,
+  	database_table_name = database_table_name,
+  	database_table_structure = database_table_structure_orig,
   ))
